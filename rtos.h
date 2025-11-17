568d9b993081be4f752e2538ad4e5c076c8ab145
Victor Cornille <victor.bolheme@gmail.com>
2025-11-17 22:23:23 +0100

    wip: right char count, wrong char

--- rtos.h
+++ rtos.h
@@ -3,24 +3,51 @@
 
 #include <stdint.h>
 
+typedef uint8_t		u8;
 typedef uint32_t	u32;
 typedef int32_t		s32;
 
-extern void	ets_printf(const char* fmt, ...);
-extern void	ets_delay_us(u32 us);
-extern void	ets_install_putc1(void (*)(char));
-extern void	uart_tx_one_char(char);
+extern void		ets_printf(const char* fmt, ...);
+extern void		ets_delay_us(u32 us);
+extern void		ets_install_putc1(void (*)(u8));
+extern void		uart_tx_one_char(u8);
+extern void		ets_update_cpu_frequency(u32 mhz);
 
-#define INTERNAL		static
-#define BIT(a)			(1 << (a))
-#define REG32(a)		(*(volatile u32*)(a))
+#define INTERNAL			static
+#define BIT(a)				(1 << (a))
+#define REG32(a)			(*(volatile u32*)(a))
 
-#define GPIO_OUT_REG		REG32(0x60000300)
-#define GPIO_OUT_SET_REG	REG32(0x60000304)
-#define GPIO_OUT_CLR_REG	REG32(0x60000308)
-#define GPIO_ENABLE_REG		REG32(0x6000030C)
+#define CLEAR_PERI_REG_MASK(reg, mask)	(REG32(reg) = REG32(reg) & (~(mask)))
+#define SET_PERI_REG_MASK(reg, mask)	(REG32(reg) = REG32(reg) | (mask))
 
-#define UART_BASE_REG(a)	(0x60000000 + (a) * 0xf00)
-#define UART_CLKDIV(a)		(UART_BASE_REG(a) + 0x14)
+#define PERI_IO_MUX			0x60000800
+#define PERI_IO_MUX_FUNC		0x13
+#define PERI_IO_MUX_FUNC_S		4
+#define PERI_IO_MUX_PULLUP		BIT(7)
+#define PERI_IO_MUX_U0TXD_U		(PERI_IO_MUX + 0x18)
+#define FUNC_U0TXD			0
+
+#define PIN_PULLUP_DISABLE(pin_name)	CLEAR_PERI_REG_MASK(pin_name, PERI_IO_MUX_PULLUP)
+#define PIN_FUNC_SELECT(pin_name, func)	\
+	do { \
+		REG32(pin_name) = REG32(pin_name) \
+		& (~(PERI_IO_MUX_FUNC << PERI_IO_MUX_FUNC_S)) \
+		| (((func & BIT(2) << 2) | (func & 3)) << PERI_IO_MUX_FUNC_S); \
+	} while (0)
+
+#define GPIO_OUT_REG			REG32(0x60000300)
+#define GPIO_OUT_SET_REG		REG32(0x60000304)
+#define GPIO_OUT_CLR_REG		REG32(0x60000308)
+#define GPIO_ENABLE_REG			REG32(0x6000030C)
+
+#define UART_BASE_REG(a)		(0x60000000 + (a) * 0xf00)
+#define UART_CLKDIV(a)			(UART_BASE_REG(a) + 0x14)
+#define UART_CONF0(a)			(UART_BASE_REG(a) + 0x20)
+#define UART_BIT_NUM			3
+#define UART_BIT_NUM_S			4
+#define UART_STOP_BIT_NUM		3
+#define UART_STOP_BIT_NUM_S		2
+#define UART_PARITY_ENABLE		BIT(1)
+#define UART_PARITY			BIT(0)
 
 #endif
--- user/main.c
+++ user/main.c
@@ -4,7 +4,7 @@
 #define BAUDRATE	115200
 
 INTERNAL void
-my_putc(char c)
+my_putc(u8 c)
 {
 	uart_tx_one_char(c);
 }
@@ -12,7 +12,16 @@ my_putc(char c)
 int
 main(void)
 {
+	PIN_PULLUP_DISABLE(PERI_IO_MUX_U0TXD_U);
+	PIN_FUNC_SELECT(PERI_IO_MUX_U0TXD_U, FUNC_U0TXD);
 	ets_update_cpu_frequency(80);
+	REG32(UART_CONF0(0))  = REG32(UART_CONF0(0)) 
+		& ~(UART_PARITY_ENABLE 
+			| UART_PARITY
+			| (UART_BIT_NUM << UART_BIT_NUM_S)
+			| (UART_STOP_BIT_NUM << UART_STOP_BIT_NUM_S))
+		| (3 << UART_BIT_NUM_S)
+		| (1 << UART_STOP_BIT_NUM_S);
 	REG32(UART_CLKDIV(0)) = 80000000 / BAUDRATE;
 	ets_install_putc1(my_putc);
 	GPIO_ENABLE_REG |= BIT(PIN_LED);
